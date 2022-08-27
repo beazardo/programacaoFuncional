2ccca74cd1fa4b6f8601243297d3a80a1fe82d24
beazardo <65186187+beazardo@users.noreply.github.com>
2022-08-26 21:18:15 -0300

    Add files via upload

--- Main.hs
+++ Main.hs
@@ -1,87 +1,81 @@
--- #Aluna: Beatriz de Assumpção Zardo#-}
+-- 1. Escreva  uma  função  para  o  cálculo  dos  números  da  sequência  de  Fibonacci,  utilizando Haskell.
+secFibonacci :: Int -> Int
+secFibonacci 0 = 0
+secFibonacci 1 = 1
+secFibonacci a = secFibonacci (a -1) + secFibonacci (a -2)
 
--- # 1. Escreva uma função chamada soma1 que recebe um inteiro como argumento e retorna um inteiro uma unidade maior que a entrada.  #-}
+-- 2. Um dos primeiros algoritmos documentados é o algoritmo para o cálculo do Maior Divisor Comum  (MDC)  de  Euclides  publicado  por  volta do  ano 300  AC.  Podemos simplificar  este algoritmo  dizendo  que  dados  dois  inteiros  A  e  B,  o  MDC  entre  eles  será dado  pelo  valor absoluto de A se B=0 e pelo MDC entre B e o resto da divisão de A por B se B>0. Escreva uma  função  para  o  cálculo  do  MDC  entre  dois  números  inteiros  positivos,  usando  o algoritmo de Euclides conforme apresentado aqui, utilizando Haskell.
+mdc :: Int -> Int -> Int
+mdc a b
+  | b == 0 = a
+  | otherwise = mdc b (mod a b)
 
-soma1 :: Int -> Int 
-soma1 x = x + 1
+-- 3. Escreva uma função recursiva que dado um número inteiro n, devolva a soma dos dígitos deste  número.  Exemplo:  dado  1234  a  função  deverá  devolver  10.  Utilizando  Haskell  e recursividade.
+somaNumeros :: Int -> Int
+somaNumeros 0 = 0
+somaNumeros a = mod a 10 + somaNumeros (a `quot` 10)
 
--- # 2. Escreva  uma  função  chamada  sempre  que,  não  importando  o  valor  de  entrada,  devolva sempre zero. Observe que neste caso a entrada pode ser de qualquer tipo.
+-- 4. Escreva  uma  função  que  devolva  a  soma  de  todos  os  números  menores  que  10000  que sejam múltiplos de 3 ou 5.
+somaMenores :: Int -> Int -> Int
+somaMenores 0 s = s
+somaMenores a s
+    | mod a 3 == 0 = somaMenores (a-1) (s + a) 
+    | mod a 5 == 0 = somaMenores (a-1) (s + a) 
+    | otherwise = somaMenores (a-1) s 
 
-sempre :: String -> Int
-sempre a = 0
+-- 5. Escreva  uma  função que,  recebendo  uma  lista  de  inteiros,  apresente  a  diferença  entre a soma dos quadrados e o quadrado da soma destes inteiros, usando recursividade. 
+diferenca :: [Int] -> Int -> Int -> Int
+diferenca [] somaQuadrados somaElevada = somaQuadrados - (somaElevada*somaElevada)
+diferenca lista somaQuadrados somaElevada = diferenca (tail lista) (somaQuadrados + (head lista)*(head lista)) (somaElevada + head lista)
 
---  3. Escreva  uma  função  chamada  treco  que  receba  três  valores  em  ponto  flutuantes  com precisão dupla e retorne o resultado da soma dos dois primeiros multiplicado pelo terceiro. # -}
+-- 6. O Crivo de Eratóstenes não é o melhor algoritmo para encontrar números primos. Crie uma função que implemente o Crivo de Euler (Euler’s Sieve) para encontrar todos os números primos menores que um determinado inteiro dado.
+criaLista :: Int -> Int -> [Int] -> Int
+criaLista nAlvo ncontador listaNumeros = criaLista nAlvo (ncontador-1) (ncontador : listaNumeros)
 
-treco :: Float -> Float -> Float -> Float
-treco v1 v2 v3  = (v1 + v2) * v3
+eliminaPrimos :: [Int] -> [Int] -> [Int]
+eliminaPrimos listaNumeros listaPrimos = []
 
--- #4. Escreva uma função chamada resto que devolva o resto de uma divisão entre dois números inteiro.#-}
+-- 7. Nem  só  de  Fibonacci  vivem  os  exemplos  de  recursão.  Escreva  uma  função  que  devolva todos os números de uma sequência de Lucas (2, 1, 3, 4, 7, 11, 18, 29, 47, 76, 123) menores que um inteiro dado.
+calculoLucas :: Int -> Int
+calculoLucas numero
+          | numero == 0 = 2
+          | numero == 1 = 1
+          | otherwise = calculoLucas (numero -1) + calculoLucas(numero - 2)
 
-resto :: Int -> Int -> Int
-resto n1 n2 = mod n1 n2
+montaListaLucas :: Int -> Int -> [Int] -> [Int]
+montaListaLucas numeroParada numero listaLucas
+      | numeroParada == 1 = []
+      | numeroParada == 2 = []
+      | calculoLucas numero > numeroParada = listaLucas
+      | otherwise = montaListaLucas numeroParada (numero + 1) listaLucas ++ [(calculoLucas numero)]
 
--- #5. Escreva uma função chamada precoMaior que devolva o maior valor entre quatro valores monetários. #-}
+-- 8. Escreva uma função, chamada aoContrario em Haskel para reverter uma lista. Dado [1,2,3] devolva [3,2,1].
+aoContrario :: [Int] -> [Int] -> [Int]
+aoContrario [] listaContraria = listaContraria
+aoContrario lista listaContraria = aoContrario (tail lista) ((head lista) : listaContraria)
 
-precoMaior :: (Ord valores) => [valores] -> valores 
-precoMaior [] = error "Lista vazia"
-precoMaior [valores] = valores
-precoMaior (x:xs)   
-    | x > maxTail = x  
-    | otherwise = maxTail  
-    where maxTail = precoMaior xs
+-- 9. Escreva uma função chamada somaRecursiva que recebe dois valores inteiros e devolve o produto destes valores sem usar o operador de multiplicação.
 
--- # 6. Escreva uma função chamada impar que devolva True, sempre que o resultado do produto de dois números inteiros for ímpar #-}
+somaRecursiva :: Int -> Int -> Int -> Int
+somaRecursiva a b soma
+  | b == 1 = soma + a
+  | otherwise = somaRecursiva a (b -1) (soma + a)
 
-impar :: [Int] -> Bool
-impar [a,b] = if even(a*b) then False else True
+-- 10. Escreva uma função chamada comprimento que receba uma lista de  inteiros e devolva o comprimento desta lista. Observe que você não pode usar nenhuma função que já calcule o comprimento de uma lista.
+comprimento :: [Int] -> Int -> Int
+comprimento [] contador = contador
+comprimento a contador = comprimento (tail a) (contador+1)
 
--- # 7. Em Haskell existe o tipo par cuja assinatura tem a seguinte forma: 𝑝𝑎𝑟∷(𝐼𝑛𝑡,𝐼𝑛𝑡). Escreva uma função em Haskell que devolva a soma dos componentes de um par de inteiros. #-}
-somaPar :: (Int, Int) -> Int
-somaPar (a, b) = a + b
-
--- # 8. Escreva uma função em Haskell que receba números reais (double) e devolva o resultado da equação 𝑥2 +𝑦2 +𝑧. #-}
-equacao :: Double -> Double -> Double -> Double
-equacao x y z = x*x + y*y +z
-
--- # 9. Escreva uma função em Haskell chamada diagnostico que receba o peso do aluno e imprima um  diagnóstico  de  obesidade,  segundo  a  tabela  que  pode  ser  encontrada  no  link: Sobrepeso,  obesidade  e  obesidade  mórbida:  entenda  a  diferença  entre  os  três  termos (cuidadospelavida.com.br).  Observe  que  este  diagnóstico  é  meramente  estatístico  e  não tem nenhum valor real, está sendo usado nesta questão apenas para a definição das faixas. Todo e qualquer diagnóstico deve ser feito por um profissional médico.  #-}
-
-diagnostico :: Double -> Double -> IO()
-diagnostico peso altura 
-    | 17 > resultado  = putStrLn "Muito abaixo do peso"
-    | 18.49 > resultado && 17 < resultado = putStrLn "Abaixo do peso"
-    | 24.99 > resultado && 18.5 < resultado = putStrLn "Peso normal"
-    | 29.99 > resultado && 25 < resultado = putStrLn "Sobrepeso"
-    | 30 > resultado && 34.99 < resultado = putStrLn "Obesidade Leve"
-    | 35 > resultado && 39.99 < resultado = putStrLn "Obesidade Severa"
-    | 40 < resultado = putStrLn "Obesidade Morbida"
-    | otherwise = putStrLn "Dados incorretos, nao foi possivel calcular"
-  where resultado = peso/(altura^2)
-
--- # 10. Escreva uma função em Haskell chamada bissexto que receba um ano e devolva True se o ano for bisexto sabendo que anos bissextos obedecem a seguinte regra:  𝑇𝑜𝑑𝑜𝑠 𝑜𝑠 𝑎𝑛𝑜𝑠 𝑞𝑢𝑒 𝑠𝑒𝑗𝑎𝑚 𝑑𝑖𝑣𝑖𝑠í𝑣𝑒𝑖𝑠 𝑝𝑜𝑟 4 𝐸𝑥𝑐𝑒𝑡𝑜 𝑜𝑠 𝑎𝑛𝑜𝑠 𝑞𝑢𝑒 𝑠ã𝑜 𝑚ú𝑙𝑡𝑖𝑝𝑙𝑜𝑠 𝑑𝑒 100 𝐸𝑥𝑐𝑒𝑡𝑜 𝑜𝑠 𝑎𝑛𝑜𝑠 𝑞𝑢𝑒 𝑠ã𝑜 𝑚ú𝑙𝑡𝑖𝑝𝑙𝑜𝑠 𝑑𝑒 400 #-}
-bissexto :: Int -> Bool
-bissexto ano 
-  | (mod ano 4) == 0 && (mod ano 100) == 0 && (mod ano 400) == 0 = True
-  | (mod ano 4) == 0 && (mod ano 100) /= 0 = True
-  | otherwise = False
-   
-
-                        
-main :: IO()
+main :: IO ()
 main = do
-  print("Func 1 - entrada: 3, resultado :  " ++ show (soma1 3))
-  print("Func 1 - entrada: -6, resultado :  " ++ show (soma1 (-6)))
-  print("Func 2 - entrada: coisa, resultado : " ++ show (sempre "coisa"))
-  print("Func 3 - entrada: 4.00 5.00 2.00, resultado: " ++ show (treco 4.00 5.00 2.00)) 
-  print("Func 4 - entrada: 10 5, resultado: " ++ show (resto 10 5))
-  print("Func 4 - entrada: 5 2, resultado: " ++ show (resto 5 2))
-  print("Func 5 - entrada: 1,2,3,4, resultado: " ++ show (precoMaior [1,2,3,4]))
-  print("Func 6 - entrada: 3 5, resultado: " ++ show (impar [3,5]))
-  print("Func 6 - entrada: 2 8, resultado: " ++ show (impar [2,8]))
-  print("Func 7 - entrada: 3 4, resultado: " ++ show (somaPar (3, 4)))
-  print("Func 8 - entrada: 2.0 5.0 8.0, resultado: " ++ show (equacao 2.0 5.0 8.0))
-  print("Func 9 - entrada: 77.0 1.80, resultado: ") 
-  diagnostico 77.0 1.80
-  print("Func 9 - entrada: 61.0 1.55, resultado: ") 
-  diagnostico 61.0 1.55
-  print("Func 10 - entrada 1996, resultado: " ++ show (bissexto 1996))
-  print("Func 10 - entrada 2022, resultado: " ++ show (bissexto 2022))
+
+  print ("Func 1 - entrada: 6, resultado :  " ++ show (secFibonacci 6))
+  print ("Func 2 - entrada: 3, resultado :  " ++ show (mdc 5 20))
+  print ("Func 3 - entrada: 1234, resultado :  " ++ show (somaNumeros 1234))
+  print ("Func 4 - entrada: 9999, resultado :  " ++ show (somaMenores 9999 0))
+  print ("Func 5 - entrada: [1,5,9], resultado :  " ++ show (diferenca [1,5,9] 0 0))
+
+  print ("Func 7 - entrada: 10 0 [], resultado :  " ++ show (montaListaLucas 10 0 []))
+  print ("Func 8 - entrada: [1234], resultado :  " ++ show (aoContrario [1,2,3,4] []))
+  print ("Func 9 - entrada: 4 5, resultado :  " ++ show (somaRecursiva 4 5 0))
+  print ("Func 10 - entrada: [1,2,3,4,5,6], resultado :  " ++ show (comprimento [1,2,3,4,5,6] 0))
